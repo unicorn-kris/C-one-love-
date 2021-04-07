238197909ee45fda8fd0854e2e7b017f10c597cf
Кристина <kristinafcb1@gmail.com>
2021-04-07 20:56:46 +0400

    3.3.2 Готов

---
+++ Task 3/Task_3_3/Task_3_3/Task_3_3_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_3_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter word: ");
+            string input = Console.ReadLine();
+            StringMethod.Language(input);
+        }
+    }
+    public static class StringMethod
+    {
+        public static void Language(this string inputString)
+        {
+            Regex englishText = new Regex(@"[A-Za-z]{1}");
+            Regex russianText = new Regex(@"[А-ЯЁа-яё]{1}");
+            Regex digitText = new Regex(@"[0-9]{1}");
+
+            MatchCollection rus = russianText.Matches(inputString);
+            MatchCollection eng = englishText.Matches(inputString);
+            MatchCollection dig = digitText.Matches(inputString);
+
+            if (rus.Count == inputString.Length)
+                Console.WriteLine("Russian word");
+            else if (eng.Count == inputString.Length)
+                Console.WriteLine("English word");
+            else if (dig.Count == inputString.Length)
+                Console.WriteLine("Number");
+            else
+                Console.WriteLine("Mixed");
+        }
+    }
+}
